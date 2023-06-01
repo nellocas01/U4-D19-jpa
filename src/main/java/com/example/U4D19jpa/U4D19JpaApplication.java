1d0503fb3eece8e8186860e4b5bddcd13b69e280
Nello Casolla <nellocasolla446@gmail.com>
2023-06-01 16:09:03 +0200

    assets

---
+++ src/main/java/com/example/U4D19jpa/U4D19JpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.U4D19jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U4D19JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U4D19JpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/U4D19jpa/U4D19JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.U4D19jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U4D19JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
